d3b65cae732459be11f3cdb5c341ea77cb9e7cb9
German Lopez <gerleven@gmail.com>
2022-10-03 16:11:52 -0300

    spiner loader component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Loader from "./components/loader";
 
 function App() {
+  const [value, setValue] = useState(0);
+
+  const handleClick = () => {
+    // setValue( value + 1); //ese value podria esta desactualizado
+    setValue((valuePrevio) => valuePrevio + 2); //Forma correcta
+  };
   return (
     <div className="App">
       <header className="App-header">
+      <Loader></Loader>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Advanced <code>React</code> course.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Actual Value: {value}</p>
+        <button onClick={handleClick}>Value++</button>
       </header>
     </div>
   );
