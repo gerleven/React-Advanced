c72d6e534c20799d4bc4fd2857bbe9e4a7001edc
German Lopez <gerleven@gmail.com>
2022-10-18 12:29:38 -0300

    Add a Header and Hook component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,66 @@
-import React, { useState } from "react";
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+
+
 import "./App.css";
 import Loader from "./components/loader";
+import Header from "./pages/Header";
 
+interface Person {
+  name: string;
+  job: {
+    company: string;
+  };
+}
 function App() {
-  const [value, setValue] = useState(0);
 
+  /*Variables de estado*/
+  const [value, setValue] = useState<number>(0);
+  const [persona, setPersona] = useState<Person>({
+    name: "german",
+    job: { company: "Microsoft" },
+  });
+  const [dias, setDias] = useState<string[]>(["Lunes", "Martes"]);
+
+  /*Handles*/
   const handleClick = () => {
     // setValue( value + 1); //ese value podria esta desactualizado
-    setValue((valuePrevio) => valuePrevio + 2); //Forma correcta
+    setValue((prevValue) => prevValue + 1); //Forma correcta
+    setPersona(
+      (prevPersona: Person) => ({ ...prevPersona, name: "joaquin" } as Person)
+    );
+    setDias((prevDias) => [...prevDias, "Miercoles"]);
   };
+
+  /*Efectos*/
+  useEffect(() => {
+    console.log("Efecto 1");
+    console.log(value);
+    console.log(persona);
+    console.log(dias);
+  });
+
+  useEffect(() => {
+    console.log("Eefcto 2");
+    const handleKeyListener = (e: KeyboardEvent) => {
+      console.log(e.code);
+    };
+
+    window.addEventListener("keydown", handleKeyListener);
+
+    const saniamiento = () => {
+      console.log("listener deleted");
+      window.removeEventListener("keydown", handleKeyListener);
+    };
+    return saniamiento;
+  }, []);
+
+  /*Views*/
   return (
     <div className="App">
       <header className="App-header">
-      <Loader></Loader>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Advanced <code>React</code> course.
-        </p>
-        <img src={logo} className="App-logo" alt="logo" />
+        <Header></Header>
+        
+        <Loader></Loader>
         <p>Actual Value: {value}</p>
         <button onClick={handleClick}>Value++</button>
       </header>
---
+++ src/pages/Hooks.tsx
@@ -0,0 +1,8 @@
+const Hooks = () => {
+    return ( <>
+    <h2>Hooks</h2>
+    
+    </> );
+}
+ 
+export default Hooks;
